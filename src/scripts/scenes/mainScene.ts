36b452fc7b75b48fa1b8b796c894ccbf62779760
oscariquelme01 <oscariquelmejato01@gmail.com>
2022-06-17 18:05:03 +0200

    implement initial slot idea

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,27 +1,68 @@
-import PhaserLogo from '../objects/phaserLogo'
 import FpsText from '../objects/fpsText'
 
+const ITEM_LENGTH = 82
+const DURATION = 300
+const NUMBER_ITEMS = 16
+const INITIAL_Y = 100
+const GAP = 58
+const END_Y = 600
+
 export default class MainScene extends Phaser.Scene {
-  fpsText
+    fpsText : FpsText
+    background : Phaser.GameObjects.Image
+    items : Phaser.GameObjects.Image[]
 
-  constructor() {
-    super({ key: 'MainScene' })
-  }
+    constructor() {
+        super({ key: 'MainScene' })
+    }
 
-  create() {
-    new PhaserLogo(this, this.cameras.main.width / 2, 0)
-    this.fpsText = new FpsText(this)
+    preload(){
+        this.load.spritesheet('slotItems', 'assets/sprites/spritesheet.png', {frameWidth: ITEM_LENGTH, frameHeight: ITEM_LENGTH, spacing: GAP, margin: 56})
+        this.load.image('background', 'assets/img/bg.png')
+    }
 
-    // display the Phaser.VERSION
-    this.add
-      .text(this.cameras.main.width - 15, 15, `Phaser v${Phaser.VERSION}`, {
-        color: '#000000',
-        fontSize: '24px'
-      })
-      .setOrigin(1, 0)
-  }
+    create() {
+        this.fpsText = new FpsText(this)
+        this.background = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'background')
+        this.items = []
 
-  update() {
-    this.fpsText.update()
-  }
+        for(let i = 0; i < NUMBER_ITEMS; i++){
+            var image = this.add.image(780, 100, 'slotItems', i)
+            image.setDepth(-1)
+            this.items[i] = image
+        }
+
+        var velocity = (END_Y - INITIAL_Y) / DURATION //px/s
+        var delay = ITEM_LENGTH / velocity
+
+        this.tweens.add({targets: this.items, y: END_Y, Ease:'power0', duration: DURATION, delay: this.tweens.stagger(delay, {}), repeat: 3, repeatDelay: delay, onComplete: this.show_result, onCompleteParams: [this]})
+
+    }
+
+    update() {
+        this.fpsText.update()
+    }
+
+    show_result(_: Phaser.Tweens.Tween, items: Phaser.GameObjects.Image[], scene: MainScene){
+        var random_results : number[] = []
+        var random_items : Phaser.GameObjects.Image[] = []
+        for(let i = 0; i < 3; i++){
+            var num_in_array = false
+            while (!num_in_array){
+                let random_num = Math.floor(Math.random()* NUMBER_ITEMS)
+
+                if (!random_results.includes(random_num)){
+                    random_results[i] = random_num
+                    num_in_array = true
+                }
+            }
+            random_items[i] = items[random_results[i]]
+            random_items[i].setY(INITIAL_Y)
+        }
+
+        /*Position results*/
+        for(let i = 2; i >= 0; i--){
+            var y = INITIAL_Y + (2+i) * (ITEM_LENGTH + 9)
+            scene.add.tween({targets: random_items[i], y: y, duration: 450, }) }
+    }
 }
