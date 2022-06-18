da845691fb65bfb13639a0bb5449ef331cc44db6
oscariquelme01 <oscariquelmejato01@gmail.com>
2022-06-18 22:05:35 +0200

    add score & project now using timeline instead of independent tweens

--- src/scripts/objects/slot.ts
+++ src/scripts/objects/slot.ts
@@ -2,7 +2,7 @@ import MainScene, { ITEM_LENGTH, NUMBER_ITEMS, NUM_SLOTS} from "../scenes/mainSc
 
 // each item will go from INITIAL_Y to END_Y in ROUND_DURATION ms for NUM_ROUNDS rounds
 const ROUND_DURATION = 300
-const INITIAL_Y = 100
+export const INITIAL_Y = 100
 const END_Y = 600
 const NUM_ROUNDS = 3
 
@@ -11,6 +11,7 @@ export default class slot {
     pos_slot: number
     x: number
     tween: Phaser.Tweens.Tween
+    results: number[]
 
     // x & y will determine the starting point of the slot 
     constructor(scene: MainScene, x: number, pos_slot: number){
@@ -21,34 +22,35 @@ export default class slot {
 
         this.scene.items[this.pos_slot] = []
 
+        // add images
         for(let i = 0; i < NUMBER_ITEMS; i++){
             var image = this.scene.add.image(this.x, INITIAL_Y, 'slotItems', i)
             image.setDepth(-1)
+            // name acts as id since the arrays will be shuffled and the position no longer serves as identifier
+            image.setName(`${i}`)
             this.scene.items[this.pos_slot][i] = image
         }
 
+        // necessary so that all the slots don't look the same
         this.shuffleArray(this.scene.items[this.pos_slot])
 
+        // Add the slot to the scene
         var velocity = (END_Y - INITIAL_Y) / ROUND_DURATION //px/s
         var delay = ITEM_LENGTH / velocity
 
-        // Add the slot to the scene
-        this.tween = this.scene.tweens.add({targets: this.scene.items[this.pos_slot], y: END_Y, Ease:'power0', duration: ROUND_DURATION, 
-            delay: this.scene.tweens.stagger(delay, {}), repeat: NUM_ROUNDS, repeatDelay: delay, 
-            onComplete: this.show_result, onCompleteParams: [this.scene, this.pos_slot], paused: true})
+        this.scene.timeline.add({targets: this.scene.items[this.pos_slot], y: {from: INITIAL_Y, to: END_Y}, 
+            duration: ROUND_DURATION, delay: this.scene.tweens.stagger(delay, {}), repeat: NUM_ROUNDS, repeatDelay: delay, 
+            onComplete: this.show_result, onCompleteParams: [this.scene, this.pos_slot], onCompleteScope: this, 
+            offset: 0, paused: true})
     }
     
-    run(){
-        if(this.scene.in_game) return
-        this.tween.resume()
-        this.tween.restart()
-    }
-
     // callback to generate and show a random result after spinning the slot
-    show_result(_: Phaser.Tweens.Tween, items: Phaser.GameObjects.Image[], scene: MainScene){
-        var random_results : number[] = []
+    show_result(_: Phaser.Tweens.Tween, items: Phaser.GameObjects.Image[]){
+
+        this.results = []
         var random_items : Phaser.GameObjects.Image[] = []
 
+        // get random items
         for(let i = 0; i < NUM_SLOTS; i++){
             var num_in_array = false
 
@@ -56,30 +58,23 @@ export default class slot {
             while (!num_in_array){
                 let random_num = Math.floor(Math.random()* NUMBER_ITEMS)
 
-                if (!random_results.includes(random_num)){
-                    random_results[i] = random_num
+                if (!this.results.includes(random_num)){
+                    this.results[i] = random_num
                     num_in_array = true
                 }
             }
 
             // ger random image 
-            random_items[i] = items[random_results[i]]
+            random_items[i] = items[this.results[i]]
             random_items[i].setY(INITIAL_Y)
         }
 
         // i = 2 because there are 3 lines, 2+i and ITEM_LENGTH + 9 act as margin to get the right values
         for(let i = 2; i >= 0; i--){
             var y = INITIAL_Y + (2+i) * (ITEM_LENGTH + 9)
-            scene.add.tween({targets: random_items[i], y: y, duration: 450, }) 
+            this.scene.add.tween({targets: random_items[i], y: y, duration: 450}) 
         }
-
-        if(scene.in_game == true){
-            scene.button.setFrame(0)
-            scene.in_game = false
-        }
-        
     }
-
 
     // Durstenfeld shuffle array algorithm 
     shuffleArray(array: any[]) {
--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -1,7 +1,7 @@
-import slot from '../objects/slot'
+import slot, { INITIAL_Y } from '../objects/slot'
 
 export const ITEM_LENGTH = 82
-export const NUMBER_ITEMS = 16
+export const NUMBER_ITEMS = 7
 const GAP = 58
 
 export const NUM_SLOTS = 3
@@ -9,28 +9,42 @@ export const NUM_SLOTS = 3
 export default class MainScene extends Phaser.Scene {
     items : Phaser.GameObjects.Image[][]
     in_game : boolean
+    score : Phaser.GameObjects.Text
     button : Phaser.GameObjects.Sprite
     num_slots : number
     slots : slot[]
+    timeline : Phaser.Tweens.Timeline
+    first_time : Boolean
 
     constructor() {
         super({ key: 'MainScene' })
         this.num_slots = 0
         this.in_game = false
+        this.first_time = true
     }
 
     preload(){
         this.load.spritesheet('slotItems', 'assets/sprites/spritesheet.png', {frameWidth: ITEM_LENGTH, frameHeight: ITEM_LENGTH, spacing: GAP, margin: 56})
         this.load.spritesheet('button', 'assets/sprites/button.png', {frameWidth: 288, frameHeight: 195})
         this.load.image('background', 'assets/img/bg.png')
+        this.load.image('scoreFrame', 'assets/img/score-frame.png')
     }
 
     create() {
         var scene = this
 
+        // add images
         this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'background')
         this.button = this.add.sprite(300, 623, 'button').setFrame(0).setInteractive()
 
+        //score text
+        this.add.image(960, 640, 'scoreFrame')
+        this.score = this.add.text(830, 605, '100000', {fontFamily : 'neon'}).setFontSize(64).setWordWrapWidth(400)
+
+        // Create timeline to store tweens
+        this.timeline = this.tweens.createTimeline({loop:-1, onLoop: this.end_turn, onLoopScope: this})
+        this.timeline.on('resume', this.reset_items, this)
+        
         // Create all the slots
         this.items = []
         var x_positions = [780, 960, 1140]
@@ -41,16 +55,61 @@ export default class MainScene extends Phaser.Scene {
             this.num_slots++;
         }
 
-        this.input.on('pointerdown', function(){
+        //listen to click on button
+        this.button.on('pointerdown', function(){
+
+            if(scene.in_game == true) return
+            scene.in_game = true
+
             // Key will be set backed to frame 0 when the slots are done
             scene.button.setFrame(1)
-            scene.slots[0].run()
-            scene.slots[1].run()
-            scene.slots[2].run()
-            scene.in_game = true
+
+            if(scene.first_time == true){
+                scene.timeline.play()
+                scene.first_time = false
+            }
+            else{
+                scene.timeline.resume()
+            }
+
+            // check results line by line
+            // for(let i = 0; i < 3; i++){
+            //     console.log(scene.slots[0].results)
+
+            //     var slot_0_result = scene.items[0][scene.slots[0].results[i]].name
+            //     var slot_1_result = scene.items[1][scene.slots[1].results[i]].name
+            //     var slot_2_result = scene.items[2][scene.slots[2].results[i]].name
+
+            //     if(slot_0_result == slot_1_result && slot_1_result == slot_2_result){
+            //         console.log('SACADAAA')
+            //     }
+            // }
+
         }, this)
 
         
+    }
+
+    // callback on loop for the main timeline 
+    end_turn(){
+        this.timeline.pause()
+
+        // This should always evaluate to true
+        if(this.in_game == true){
+            this.button.setFrame(0)
+            this.in_game = false
+        }
+    }
+
+    // callback on resume for the main timeline. Resets back all items for the next round. 
+    // This is needed because the slot changes the y coordinate of the randomly picked items 
+    reset_items(){
+        
+        for(let j = 0; j < NUM_SLOTS; j++){
+            for(let i = 0; i < NUMBER_ITEMS; i++){
+                this.items[j][i].setY(INITIAL_Y)
+            }
+        }
     }
 
     update() {
